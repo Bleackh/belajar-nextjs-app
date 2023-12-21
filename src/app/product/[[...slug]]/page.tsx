96d92eadf267adedff3cfa65e5b8bb5c3edfc336
Bleackh <faldhi.works@gmail.com>
2023-12-21 17:28:18 +0800

    Belajar Part 5

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -8,11 +8,11 @@ export default function DetailProductPage(props: DetailProductPage) {
         <div>
             <h1>{params.slug ? "Detail Product" : "List Product"}</h1>
             {params.slug && (
-                <>
+                <div>
                     <p>Category : {params.slug[0]}</p>
                     <p>Gender : {params.slug[1]}</p>
                     <p>ID : {params.slug[2]}</p>
-                </>
+                </div>
             )}
         </div>
     )
